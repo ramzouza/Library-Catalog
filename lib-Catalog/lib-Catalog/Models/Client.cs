a8a455536de3e3d9d5210ac0e2a734ce1d4549d6
Ramez Zaid <ramezzaid@outlook.com>
2018-09-27 17:53:01 -0400

    fixed the User and Client models

--- lib-Catalog/lib-Catalog/Models/Client.cs
+++ lib-Catalog/lib-Catalog/Models/Client.cs
@@ -5,22 +5,8 @@ using System.Web;
 
 namespace lib_Catalog.Models
 {
-    abstract public class Client
+    abstract public class Client : User
     {
-        public int Id { get; set; }
-
-        public string Password_hash { get; set; }
-
-        public bool IsActive { get; set; }
-
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-
-        public string PhysicalAddress { get; set; }
-
-        public string Email { get; set; }
-
-        public string PhoneNumber { get; set; }
+        
     }
 }
