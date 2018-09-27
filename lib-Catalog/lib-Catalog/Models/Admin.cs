e123f21475db1d0010e7837afa307ff0ddf8e577
Ramez Zaid <ramezzaid@outlook.com>
2018-09-27 17:49:12 -0400

    Added models for Admin/ Clients/ Users

---
+++ lib-Catalog/lib-Catalog/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lib_Catalog.Models
+{
+    public class Admin : User
+    {
+
+    }
+}
---
+++ lib-Catalog/lib-Catalog/Models/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lib_Catalog.Models
+{
+    abstract public class Client
+    {
+        public int Id { get; set; }
+
+        public string Password_hash { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhysicalAddress { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
