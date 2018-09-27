ed58e1cfae76d1c02ef7bb0e1905db02e3bb01d6
Unknown <jonathan.mongeau@mail.concordia.ca>
2018-09-27 18:48:01 -0400

    Compiled all Models together.

---
+++ lib-Catalog/lib-Catalog/Models/Status.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lib_Catalog.Models
+{
+    public class Status
+    {
+        public Status(bool status, string msg)
+        {
+            this.status = status;
+            this.message = msg;
+        }
+        public bool status { get; set; }
+
+        public string message { get; set; }
+    }
+}
