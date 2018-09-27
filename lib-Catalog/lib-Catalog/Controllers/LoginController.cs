7445d38a7c282d04de805fc34d0465324aa783da
Sibachir Ahmed-Yanis <ahmed.yanis.sibachir@gmail.com>
2018-09-27 18:56:13 -0400

    Start doing the front-end page for the  Login

---
+++ lib-Catalog/lib-Catalog/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lib_Catalog.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult LoginUser()
+        {
+            return View();
+        }
+    }
+}
