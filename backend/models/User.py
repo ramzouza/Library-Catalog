e988ae13b60b52342b454fdb956bc3bc458b46dc
Ramez Zaid <ramezzaid@outlook.com>
2018-09-27 22:42:56 -0400

    added User model

---
+++ backend/models/User.py
@@ -0,0 +1,11 @@
+class User():
+   def __init__ (self,id,password_hash,isActive,firstName,lastName,physicalAddress,email,phoneNumber ): 
+        self.id = id
+        self.password_hash = password_hash
+        self.firstName = firstName
+        self.lastName = lastName
+        self.physicalAddress = physicalAddress
+        self.email = email
+        self. phoneNumber = phoneNumber
+        
+
