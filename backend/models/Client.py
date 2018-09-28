61f99352f42f7f00f3596b2c8dff84feaddb9894
Skander BM <skander96@hotmail.com>
2018-09-27 23:03:39 -0400

    Added Client Class

---
+++ backend/models/Client.py
@@ -0,0 +1,7 @@
+
+class Client(User):
+
+	def __init__(self,id, password,isActive, firstName, lastName,physicalAddress, email, phoneNumber):
+		User.__intit__(self,id, password,isActive, firstName, lastName,physicalAddress, email, phoneNumber)
+	
+
