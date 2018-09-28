077c0dc5aa3b6d21c5a10bb2aaa3e222311bd0d2
Yann Cedric <ngadeuyann@yahoo.fr>
2018-09-27 21:54:21 -0400

    Added few pages

---
+++ frontend/src/NewUser.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class NewUser extends Component {
+  render() {
+    return (
+      <div style={main} >
+        <span style={{fontWeight: 'bold'}} >New User</span>
+        
+        <input style={input} type="text" placeholder="Username" ></input>
+        
+        <input style={input} type="text" placeholder="Password" ></input>
+        
+        <input style={input} type="text" placeholder="Email" ></input>
+        
+        <input style={input} type="text" placeholder="Address" ></input>
+        
+        <div style={{display: 'flex', alignItems:'center'}}>
+            Admin 
+            <input type="checkbox"></input>
+        </div>
+        
+        <button style={button} type="button">Create</button>
+
+      </div>
+    );
+  }
+}
+
+export default NewUser;
+const  main = {
+    display:'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    fontFamily: 'Impact',
+    borderRadius: 5,
+    minHeight: 400,
+    fontSize: 30,
+    padding: 30,
+    boxShadow: '0px 0px 30px rgba(0,0,0,0.1)',
+    backgroundColor: 'rgba(244,244,244,0.7)',
+    marginTop: '10%',
+    // width: 200,
+}
+
+const input = {
+    fontSize: 30,
+    textAlign: 'center',
+    padding: 10,
+    borderRadius: 5,
+}
+
+const button = {
+    height: 50,
+    width: '100%',
+    fontSize: 20,
+    borderRadius: 5,
+    fontFamily: 'inherit',
+    // padding: '0px 10px',
+    // boxShadow: '0px 5px 5px rgba(0,0,0,0.5)',
+
+}
---
+++ frontend/src/Search.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div style={main}>
+        <input style={input} type="text" placeholder="Search for a Product .." ></input>
+        <button style={button} type="button">Search</button>
+      </div>
+    );
+  }
+}
+
+export default Search;
+const  main = {
+    minWidth: '30%',
+    display:'flex',
+    flexDirection: 'row',
+    alignItems:'center',
+    
+}
+
+const input = {
+    fontSize: 30,
+    padding: '15px 15px',
+    textAlign: 'left',
+    fontFamily: 'Impact',
+    borderRadius: 2,
+    width: '100%'
+}
+const button = {
+  fontSize: 20,
+  borderRadius: 5,
+  fontFamily: 'inherit',
+  padding: '15px 10px',
+  margin: 10,
+  // boxShadow: '0px 5px 5px rgba(0,0,0,0.5)',
+
+}
