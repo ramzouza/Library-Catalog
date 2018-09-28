6da135cced1c38e3510f24c0d1aa40a57cef75a8
Ramez Zaid <ramezzaid@outlook.com>
2018-09-28 17:10:15 -0400

    removed wrong .py classes and added AuthorizationService.js class

--- backend/models/Client.py
+++
@@ -1,2 +0,0 @@
-class Client extends User{
-}
--- backend/models/DbConnection.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-connection = pymysql.connect(host='18.221.83.136',user='soen343',passwd='ribalestbeau',db='mysql',port=3306)
-cursor = connection.cursor()
-sql=("")
-cursor.execute(sql)
-connection.commit()
-data=cursor.fetchall()
-print(data)
