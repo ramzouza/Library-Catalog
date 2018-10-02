9db410693fa02b04f55ec991e7aa727ffdf3c468
Yann Cedric <ngadeuyann@yahoo.fr>
2018-10-01 23:14:06 -0400

    Update on show logged users

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -11,41 +11,42 @@ class Login extends Component {
     }
   }
 
+  loginEvent(e){
+    const {email, password} = this.state
+    apiCall('/login', {email, password})
+      .then(response => response.json())
+      .then( response => {
+        alert(response.message)
+
+        if(response.status === 0){
+          
+          const isAdmin = response.results.isAdmin === 1 ? 'yes' : 'no'
+          const id = response.results.id
+
+          cookie.save('admin', isAdmin)
+          cookie.save('id', id)
+          cookie.save('email', email)
+          cookie.save('logged', 'yes')
+          apiCall('/connect', {id})
+          this.props.history.push('/')
+        }
+      }).catch(e => alert('error, make sure the backend is running'))
+  }
+
   render() {
     return (
       <div style={main} >
         <span style={{fontWeight: 'bold'}} >Login</span>
         
-        <input onChange={evt => {this.setState({email: evt.target.value})}} style={input} type="text"  placeholder="Email" ></input>
+        <input onChange={evt => {this.setState({email: evt.target.value})}} style={input} type="text"  placeholder="Email" onKeyPress={ ({key}) => key==='Enter'?this.loginEvent():null} />
         
-        <input onChange={evt => {this.setState({password: evt.target.value})}} style={input} type="password"  placeholder="Password" ></input>
+        <input onChange={evt => {this.setState({password: evt.target.value})}} style={input} type="password"  placeholder="Password" onKeyPress={ ({key}) => key==='Enter'?this.loginEvent():null} />
         
         <span style={{width:'100%', padding: '0px 40px', textAlign: 'right', fontSize: 15}}>
-            Password Forgot ?
+            Password Forgotten?
         </span>
 
-          <button style={button} type="button" onClick={ _ => {
-            const {email, password} = this.state
-            apiCall('/login', {email, password})
-              .then(response => response.json())
-              .then( response => {
-                alert(response.message)
-
-                if(response.status === 0){
-                  
-                  const isAdmin = response.results.isAdmin === 1 ? 'yes' : 'no'
-                  const id = response.results.id
-
-                  cookie.save('admin', isAdmin)
-                  cookie.save('id', id)
-                  cookie.save('email', email)
-                  cookie.save('logged', 'yes')
-                  apiCall('/connect', {id})
-                  this.props.history.push('/')
-                }
-
-              })
-          }}>
+          <button style={button} type="button" onClick={ this.loginEvent.bind(this) }>
             Login
           </button>
 
