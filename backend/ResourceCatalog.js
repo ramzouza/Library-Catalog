2ab78703d0b11482cfa33ed7dfeef46a137174bf
Ribal Aladeeb <aladeeb.r@hotmail.com>
2018-10-07 21:15:48 -0400

    Change the Global variable esources[] to books[]
    Best to keep an array of each type of resource.

--- backend/ResourceCatalog.js
+++ backend/ResourceCatalog.js
@@ -1,14 +1,14 @@
 const objectToQueryString = require("./UserCatalog").objectToQueryString
 const Book = require('./Book')
-let resources=[]
+let books=[]
 
 class ResourceCatalog {
   static MakeNewBook(book_data) {
     const newBook = new Book(book_data)
-    if (resources[newBook.ISBN_10]) {
+    if (books[newBook.ISBN_10]) {
       return { status: 1, message: "A book with the same ISBN 10 already exists" }
     }
-    resources[newBook.ISBN_10] = newBook
+    books[newBook.ISBN_10] = newBook
     console.log('resources -->', resources)
     return { status: 0, message: "Saved book" }
   }
