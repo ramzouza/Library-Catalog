d60bb9ba6e3df3eab43c4b15ecd59007b4b56931
GitBsrc <berfin.saricam@gmail.com>
2018-10-24 19:34:20 -0400

    Implemented Insert method in Resource Mapper

--- backend/ResourceMapper.js
+++ backend/ResourceMapper.js
@@ -1,5 +1,5 @@
 const MySql = require('sync-mysql')
-const User = require('./User')
+const Resource = require('./Resource')
 
 const connection = new MySql({
     host: '18.221.83.136',
@@ -10,7 +10,28 @@ const connection = new MySql({
 
 class ResourceMapper {
     static insert(resource_data) {
+        resource_data.id = 0; // For query
+        const res = new Resource(resource_data);
+        const found = this.find(res.id);
 
+        if(found){
+            try{
+            const data = connection.query(`UPDATE resources SET instances = 1 + instances WHERE id = '${resource_data.id}'`)
+            return {status: 0, message: 'Ok', results: data}
+            }
+            catch (error){
+                return{ status: 1, message: 'Error' +error, error}
+            }
+        }
+        else {
+            try{
+            const data2 = connection.query(`INSERT INTO resources VALUES (${this.objectToQueryString(res)})`)
+            return {status: 0, message: 'Ok', results: data2}
+            }
+            catch(error){
+                return{ status: 1, message: 'Error' +error, error}
+            }
+        }
     }
 
     static delete(resource_data) {
