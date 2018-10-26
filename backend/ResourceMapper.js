c272fef60a6f2b0deb6a56a0a1d511be5d8049cb
Eric Kokmanian <erickokmanian1997@gmail.com>
2018-10-25 20:21:16 -0400

    Update ResourceMapper's delete and update methods

--- backend/ResourceMapper.js
+++ backend/ResourceMapper.js
@@ -8,16 +8,18 @@ const connection = new MySql({
     database: 'mysql',
 })
 
+// ResourceMapper class that will make calls to the database whenever a resource needs to be added, updated or deleted
 class ResourceMapper {
+
+    //Method to insert resource into resources table
     static insert(resource_data) {
-        resource_data.id = 0; // For query
         const res = new Resource(resource_data);
         const found = this.find(res.id);
 
         if(found){
             try{
-            const data = connection.query(`UPDATE resources SET instances = 1 + instances WHERE id = '${resource_data.id}'`)
-            return {status: 0, message: 'Ok', results: data}
+                const data = connection.query(`UPDATE resources SET instances = 1 + instances WHERE id = '${resource_data.id}'`)
+                return {status: 0, message: 'Ok', results: data}
             }
             catch (error){
                 return{ status: 1, message: 'Error' +error, error}
@@ -25,8 +27,8 @@ class ResourceMapper {
         }
         else {
             try{
-            const data2 = connection.query(`INSERT INTO resources VALUES (${this.objectToQueryString(res)})`)
-            return {status: 0, message: 'Ok', results: data2}
+                const data2 = connection.query(`INSERT INTO resources VALUES (${this.objectToQueryString(res)})`)
+                return {status: 0, message: 'Ok', results: data2}
             }
             catch(error){
                 return{ status: 1, message: 'Error' +error, error}
@@ -34,11 +36,30 @@ class ResourceMapper {
         }
     }
 
+    // This method will make a sql call to delete an entire resource record once called
     static delete(resource_data) {
+        try{
+            const data = connection.query(`DELETE FROM resources where id=${resource_data.id}`);
+            return {status : 0, message : 'Resource deleted.', results : data}
+        }
+        catch(error){
+            return {status : 1, message : 'Error' +error, error}
+        }
 
     }
 
+    // Method to update relevant resource given the new information
     static update(resource_data) {
+        try {
+            const data = connection.query(`UPDATE resources VALUES (${this.objectToQueryString(resource_data)}) WHERE id='${resource_data.id}'`);
+            return {status : 0, message : 'Resource updated.', results : data}
+        }
+        catch(error) {
+            return {status : 1, message : 'Error' +error, error}
+        }
+    }
 
+    static objectToQueryString(object) { // Helper. This method turns an object into a string formated for an SQL query
+        return Object.values(object).map(x => "'" + x + "'").join(',');
     }
 }
