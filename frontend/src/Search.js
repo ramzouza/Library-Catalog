21ca56d1f41f7f250f8b9d31ab51976324231b10
GitBsrc <berfin.saricam@gmail.com>
2018-11-01 15:15:29 -0400

    Created Advanced Search pop up and filters. Added bootstrap.

--- frontend/src/Search.js
+++ frontend/src/Search.js
@@ -9,20 +9,56 @@ class Search extends Component {
       resource_list: []
     }
   }
-
+  
   render() {
     const {resource_list} = this.state;
     console.log(resource_list);
     return (
       <div>
-      <div style={main}>
-        <input style={input} type="text" placeholder="Search for a resource ..." ></input>
-        <button ><img alt="advanced search" src= "../../images/advancedSearch.png" /> </button>
+        <div style={main}>
+          <input style={input} type="text" placeholder="Search for a resource ..." ></input>
+          <button  data-toggle="modal" data-target="#myModal" ><img alt="advanced search" src= "../images/advancedSearch.png" /> </button>
         </div>
+
         <div>
-      <button style={button} type="button">Search</button>
-      </div>
+          <button style={button} type="button">Search</button>
+        </div>
+
+        <div>
+
+        <div class="container">
+
+          <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+            
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Advanced Search</h4>
+                </div>
+                <div class="modal-body">
+                 <span>Title  : </span><input></input><br></br>
+                 <span>Author : </span><input></input><br></br>
+                 <span>ISBN : </span><input></input><br></br>
+                 <span>Type : </span>
+                 <input type="checkbox" name="book" value="book" /> Book &nbsp;
+                 <input type="checkbox" name="magazine" value="magazine" /> Magazine &nbsp;
+                 <input type="checkbox" name="movie" value="movie" /> Movie &nbsp;
+                 <input type="checkbox" name="music" value="music" /> Music &nbsp;
+                 
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Search</button>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          
+        </div>
+        </div>
      </div>
+     
     );
   }
 }
