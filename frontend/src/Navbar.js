e56ed4fd568f22b867be6f18c475f27b89e04a59
skander ben mekiki <skander96@hotmail.com>
2018-11-15 22:52:18 -0500

    New Navbar completed

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -3,51 +3,86 @@ import {Link} from 'react-router-dom'
 import { Redirect } from 'react-router-dom';
 import cookie from 'react-cookies'
 import { apiCall } from './ApiCall';
-import { Glyphicon,MenuItem, DropdownButton, ButtonToolbar,Image,Dropdown } from 'react-bootstrap';
+import { Glyphicon,MenuItem, DropdownButton, ButtonToolbar,Image,Dropdown,SplitButton } from 'react-bootstrap';
 
 
 class Navbar extends Component {
 
-
+Test(){
+  const id = cookie.load('id')  
+    cookie.remove('logged') 
+    cookie.remove('admin') 
+    cookie.remove('email')
+    cookie.remove('id')
+    
+    alert('Goodbye!')
+    apiCall('/disconnect',{id})
+    window.location.reload();
+}
 
 
   
   render() {
     const logged = cookie.load('logged') === 'yes'
     const admin = cookie.load('admin') === 'yes'
-
-    
-    let create_resource = '/create'
-    let create_user = '/create/user' 
-    let logged_users = '/loggedusers'
-    let view_cart = '/cart' 
-
     
     return (
       
       
-    <div class="navbar-main" >
+    <div class='navbar-main' >
 
-    
-    <ButtonToolbar>
-    <DropdownButton
+    <div class='menu'>
+     { admin?
+   ( <ButtonToolbar>
+    <SplitButton
+    bsStyle="default"
+    title={<Glyphicon glyph='list'/> }
+    pullRight
+    id="split-button-dropup-pull-right"  
+    >
+    <MenuItem eventKey="1"  > <Link to="/" >
+      <Glyphicon glyph='home'/> Home
+            </Link></MenuItem>
+      <MenuItem eventKey="2"  > <Link to="/create/user" >
+      <Glyphicon glyph='user'/> Create user
+            </Link></MenuItem>
+      <MenuItem eventKey="3" ><Link to='/create'>
+          <Glyphicon glyph='pencil'/> Create Resource
+            </Link></MenuItem>
+      <MenuItem eventKey="4" ><Link to='/loggedusers'>
+      <Glyphicon glyph='eye-open'/> View Logged Users
+            </Link></MenuItem>
+      <MenuItem eventKey="5" ><Link to='/cart'>
+      <Glyphicon glyph='shopping-cart'/> View Cart
+            </Link></MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey="6" onClick={this.Test}><Link to='/login'>
+      <Glyphicon glyph='log-out'/>  Log Off
+            </Link>
+            </MenuItem>
+     
+    </SplitButton>
+  </ButtonToolbar>)
+  :    (
+  <ButtonToolbar>
+   <DropdownButton
     bsStyle="default"
     title={<Glyphicon glyph='list'/>}
     noCaret
-    pullRight
     id="dropdown-no-caret"  
     >
-      <MenuItem eventKey="1">Action</MenuItem>
-      <MenuItem eventKey="2">Another actionaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</MenuItem>
-      <MenuItem eventKey="3" active>
-        Active Item
-      </MenuItem>
-      <MenuItem divider />
-      <MenuItem eventKey="4">Separated link</MenuItem>
-     
-    </DropdownButton>
-  </ButtonToolbar>
-
+    
+    <MenuItem eventKey="1" ><Link to='/'>
+              Home
+          </Link></MenuItem>
+    <MenuItem divider />
+    <MenuItem eventKey="2" onSelect={this.Test}><Link to='/login'>
+    <Glyphicon glyph='log-out'/>  Log Off
+            </Link></MenuItem>
+   
+  </DropdownButton>
+</ButtonToolbar>)}
+</div>
 
   {/*
 
